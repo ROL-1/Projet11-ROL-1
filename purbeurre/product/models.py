3b598e5108b415040d546534719b3721ef471aab
ROL-1 <loicromero1@gmail.com>
2020-12-30 11:16:32 +0100

    fix(app:product) models fixs

--- purbeurre/product/models.py
+++ purbeurre/product/models.py
@@ -1,25 +1,7 @@
 from django.db import models
 
-class Product(models.Model):
-    product_name_fr = models.CharField(max_length=100)
-    generic_name_fr = models.CharField(max_length=100)
-    description = models.TextField()
-    fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
-    satured-fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
-    salt_100g = models.DecimalField(max_digits=5, decimal_places=2)
-    sugars_100g = models.DecimalField(max_digits=5, decimal_places=2)
-    url = models.URLField()
-    image_url = models.URLField()
-    CodesProductsOff = models.ForeignKey(CodesProductsOff, on_delete=models.CASCADE)
-    Brands = models.ForeignKey(Brands, on_delete=models.DO_NOTHING)
-    NutriscroreGrades = models.ForeignKey(NutriscroreGrades, on_delete=models.DO_NOTHING)
-    Categories = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-       return self.product_name_fr
-
 class CodesProductsOff(models.Model):
-    code = PositiveBigIntegerField()
+    code = models.PositiveBigIntegerField()
 
 class Brands(models.Model):
     brands = models.CharField(max_length=75)
@@ -33,3 +15,21 @@ class Categories(models.Model):
 
     def __str__(self):
        return self.category
+
+class Product(models.Model):
+    product_name_fr = models.CharField(max_length=100)
+    generic_name_fr = models.CharField(max_length=100)
+    description = models.TextField()
+    fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    satured_fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    salt_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    sugars_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    url = models.URLField()
+    image_url = models.URLField()
+    CodesProductsOff = models.ForeignKey(CodesProductsOff, on_delete=models.CASCADE)
+    Brands = models.ForeignKey(Brands, on_delete=models.DO_NOTHING)
+    NutriscroreGrades = models.ForeignKey(NutriscroreGrades, on_delete=models.DO_NOTHING)
+    Categories = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+       return self.product_name_fr
