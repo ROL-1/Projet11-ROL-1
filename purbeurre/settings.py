d15e9df704a98005faae990545ac0708265a0bee
ROL-1 <loicromero1@gmail.com>
2021-01-07 13:10:25 +0100

    heroku setttings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -140,21 +140,19 @@ STATIC_URL = "/static/"
 
 INTERNAL_IPS = ["127.0.0.1"]
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 if os.environ.get("ENV") == "PRODUCTION":
 
-    # Static files settings
+    # # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
-    # Extra places for collectstatic to find static files.
+    # # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = (
-        "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    )
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES["default"].update(db_from_env)
