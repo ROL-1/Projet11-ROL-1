ea532aff802adf4598b205a956e6e8d976b5acd5
ROL-1 <loicromero1@gmail.com>
2021-01-11 12:01:01 +0100

    templates(creation) add account.html, log.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^results/", views.results),
+    url(r"^account/", views.account),
+    url(r"^log/", views.log),
     url(r"^product/", views.product),
+    url(r"^results/", views.results),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -5,8 +5,23 @@ from django.template import loader
 # Create your views here.
 
 
+def account(request):
+    template = loader.get_template("webapp/account.html")
+    return HttpResponse(template.render(request=request))
+
+
 def index(request):
     template = loader.get_template("webapp/index.html")
+    return HttpResponse(template.render(request=request))
+
+
+def product(request):
+    template = loader.get_template("webapp/product.html")
+    return HttpResponse(template.render(request=request))
+
+
+def log(request):
+    template = loader.get_template("webapp/log.html")
     return HttpResponse(template.render(request=request))
 
 
@@ -14,7 +29,3 @@ def results(request):
     template = loader.get_template("webapp/results.html")
     return HttpResponse(template.render(request=request))
 
-
-def product(request):
-    template = loader.get_template("webapp/product.html")
-    return HttpResponse(template.render(request=request))
