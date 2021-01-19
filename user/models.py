eef2649dbd69207862d9982e0070052fbbe874d1
ROL-1 <loicromero1@gmail.com>
2021-01-19 10:22:14 +0100

    add scss

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# from django.forms import forms
+from django.forms import forms
 
 
 # class Users(models.Model):
