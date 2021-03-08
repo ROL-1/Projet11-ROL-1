85e690922b47385ed0f0016379bdb4cc43dcdfc6
ROL-1 <loicromero1@gmail.com>
2021-03-08 10:25:08 +0100

    remove secret key

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -24,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "ya^^!m@+9t14#tv*_cw4ejpigi=@p1vu3_%nh+lrhhtf"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY",)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
