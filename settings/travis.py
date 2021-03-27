543b9bef4736adae04b2eb3ae5986300ddf8c577
ROL-1 <loic.romero1@gmail.com>
2021-03-27 13:05:01 +0000

    update travis config

--- settings/travis.py
+++ settings/travis.py
@@ -3,10 +3,10 @@ from . import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
+        'NAME': 'purbeurre',
         'USER': 'rol',
-        'PASSWORD': '',
+        'PASSWORD': 'mdp123mdp',
         'HOST': '',
-        'PORT': '',
+        'PORT': '5432',
     },
 }
