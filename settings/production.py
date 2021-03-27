a38e1a099357e09631741db30c0176eea2972a6b
ROL-1 <loic.romero1@gmail.com>
2021-03-27 13:48:50 +0000

    sentry config

--- settings/production.py
+++ settings/production.py
@@ -13,7 +13,27 @@ DATABASES = {
         'HOST': '',
         'PORT': '5432',
     }
+
 }
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://04ef3937c89c464c9447bdec16877d53@o559490.ingest.sentry.io/5695055",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+
 import raven
 
 INSTALLED_APPS += [
