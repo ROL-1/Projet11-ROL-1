8b8862bd897c63f3110e09d16073db298ebe2f6b
ROL-1 <loicromero1@gmail.com>
2021-04-18 20:41:03 +0200

    feature(filters) add tests

--- tests/integration/webapp/tests_views.py
+++ tests/integration/webapp/tests_views.py
@@ -204,7 +204,10 @@ class TestWiews(TestCase):
         
         2. Check if suitables templates are used.
         """
-        response = self.client.get(reverse("search"), {"query": self.query})
+        response = self.client.get(
+            reverse("search"),
+            {"nutri_filter": self.nutri_filter, "query": self.query},
+        )
         self.assertEqual(response.status_code, 200)
         with self.assertTemplateUsed("webapp/search.html"):
             render_to_string("webapp/search.html")
@@ -213,6 +216,18 @@ class TestWiews(TestCase):
         with self.assertTemplateUsed("webapp/portfoliobox.html"):
             render_to_string("webapp/search.html")
         self.assertTrue(len(response.context["products"]) > 1)
+        self.assertTrue(response.context["nutri_filter"] == "5")
+
+    def test_if_view_search_filter_products_and_context_is_filled(self):
+        """1. Check if products are filtered.
+        
+        2. Check if context is filled correctly."""
+        response = self.client.get(
+            reverse("search"), {"nutri_filter": 1, "query": self.query},
+        )
+        self.assertTrue(response.context["nutri_filter"] == "1")
+        self.assertTrue(str(response.context["products"]) == "<Page 1 of 1>")
+        self.assertTrue(str(response.context["nutriscore_list"]) == "[1]")
 
     def test_myFavorites_page_name_is_not_mesFavoris(self):
         """Check if NoReverseMatch is caused by page named mesFavoris."""
